46a685b1d6466665e419bfd5f0f9afcde9b95cbc
Amirhossein Azizafshari <a_amirhossein@icloud.com>
2021-03-06 02:03:02 -0500

    First Commit

---
+++ src/main/java/com/azizafsh/heartinsync/HeartinsyncApplication.java
@@ -0,0 +1,13 @@
+package com.azizafsh.heartinsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartinsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartinsyncApplication.class, args);
+    }
+
+}
